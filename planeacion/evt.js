eebbd1a905458f678c8d4c71130b5ce6b6432981
geoproyecciones <geoproyecciones@Julian>
2017-08-31 19:21:08 -0500

    tablas 100%

--- planeacion/evt.js
+++ planeacion/evt.js
@@ -51,7 +51,6 @@ function filtro() {
     table = document.getElementById("dintab");
     tr = table.getElementsByTagName("tr");
     var index = document.getElementById("selcolum").selectedIndex;
-    //console.log(tr[0].getElementsByTagName("td")[0]);
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 1; i < tr.length; i++) {
         td = tr[i].getElementsByTagName("td")[index];
@@ -74,96 +73,129 @@ function showtable(capas) {
     var x = document.getElementById("selcolum");
     x.innerHTML = "";
     if (capas.target.innerHTML === "Barrios") {
-
+        var colum = search("preproduccion:NameColumns", 'barrios');
+        var sel = search("preproduccion:selallbarrios");
     } else if (capas.target.innerHTML === "Localidades") {
-
+        var colum = search("preproduccion:NameColumns", 'localidades');
+        var sel = search("preproduccion:selalllocalidades");
     } else if (capas.target.innerHTML === "Amenaza Inundación") {
         var colum = search("preproduccion:NameColumns", 'amenazainundacion');
-        var option = [];
-        for (i = 0; i < colum.length; i++) {
-            option[i] = document.createElement("option");
-            option[i].text = colum[i];
-            x.add(option[i], i);
-        }
         var sel = search("preproduccion:selallamenazainundacion");
-        for (j = 0; j < colum.length; j++) {
-            cell1 = row.insertCell(j);
-            cell1.innerHTML = colum[j];
-        }
-        for (j = 0; j < sel.length; j++) {
-            var row = table.insertRow(j + 1);
-            for (k = 0; k < sel[0].length; k++) {
-                cell1 = row.insertCell(k);
-                cell1.innerHTML = sel[j][k];
-            }
-        }
     } else if (capas.target.innerHTML === "Remoción en Masa") {
-
+        var colum = search("preproduccion:NameColumns", 'amenazaremocionmasa');
+        var sel = search("preproduccion:selallamenazaremocionmasa");
     } else if (capas.target.innerHTML === "Area Reserva Acueducto") {
-
+        var colum = search("preproduccion:NameColumns", 'areas_reservaacueducto');
+        var sel = search("preproduccion:selallareas_reservaacueducto");
     } else if (capas.target.innerHTML === "Area Reserva Alcantarillado") {
-
+        var colum = search("preproduccion:NameColumns", 'areasreservaalcantarillado');
+        var sel = search("preproduccion:selallareasreservaalcantarillado");
     } else if (capas.target.innerHTML === "Arroyos") {
-
+        var colum = search("preproduccion:NameColumns", 'arroyos');
+        var sel = search("preproduccion:selallarroyos");
     } else if (capas.target.innerHTML === "Ciclorutas Propuestas") {
-
+        var colum = search("preproduccion:NameColumns", 'ciclorutas_propuestas');
+        var sel = search("preproduccion:selallciclorutas_propuestas");
     } else if (capas.target.innerHTML === "Cienaga Mallorquin") {
-
+        var colum = search("preproduccion:NameColumns", 'cienagamallorquin');
+        var sel = search("preproduccion:selallcienagamallorquin");
     } else if (capas.target.innerHTML === "Clasificación Suelo") {
-
+        var colum = search("preproduccion:NameColumns", 'clasificacionsuelo');
+        var sel = search("preproduccion:selallclasificacionsuelo");
     } else if (capas.target.innerHTML === "Estaciones Transmetro") {
-
+        var colum = search("preproduccion:NameColumns", 'estacionestransmetro');
+        var sel = search("preproduccion:selallestacionestransmetro");
     } else if (capas.target.innerHTML === "Espacio Público") {
-
+        var colum = search("preproduccion:NameColumns", 'espacio_publico');
+        var sel = search("preproduccion:selallespacio_publico");
     } else if (capas.target.innerHTML === "Limites Distrital") {
-
+        var colum = search("preproduccion:NameColumns", 'limitedistrital');
+        var sel = search("preproduccion:selalllimitedistrital");
     } else if (capas.target.innerHTML === "Malecon Avenida Río") {
-
+        var colum = search("preproduccion:NameColumns", 'maleconavenidario');
+        var sel = search("preproduccion:selallmaleconavenidario");
     } else if (capas.target.innerHTML === "Perfil Vias Urbanas Propuestas") {
-
+        var colum = search("preproduccion:NameColumns", 'maleconavenidario');
+        var sel = search("preproduccion:selallmaleconavenidario");
     } else if (capas.target.innerHTML === "Parques Propuestos") {
-
+        var colum = search("preproduccion:NameColumns", 'parquespropuestos');
+        var sel = search("preproduccion:selallparquespropuestos");
     } else if (capas.target.innerHTML === "Parques Propuestos Rural") {
-
+        var colum = search("preproduccion:NameColumns", 'parquespropuestosrural');
+        var sel = search("preproduccion:selallparquespropuestosrural");
     } else if (capas.target.innerHTML === "Perimetro Urbano") {
-
+        var colum = search("preproduccion:NameColumns", 'perimetrourbano');
+        var sel = search("preproduccion:selallperimetrourbano");
     } else if (capas.target.innerHTML === "Instrumentos Planificación") {
-
+        var colum = search("preproduccion:NameColumns", 'poligonosinstrumentosplanificacion');
+        var sel = search("preproduccion:selallpoligonosinstrumentosplanificacion");
     } else if (capas.target.innerHTML === "Normativos Uso") {
-
+        var colum = search("preproduccion:NameColumns", 'poligonosnormativos');
+        var sel = search("preproduccion:selallpoligonosnormativos");
     } else if (capas.target.innerHTML === "Protección Litoral Mar Caribe") {
-
+        var colum = search("preproduccion:NameColumns", 'proteccionlitoralmarcaribe');
+        var sel = search("preproduccion:selallproteccionlitoralmarcaribe");
     } else if (capas.target.innerHTML === "Protección Conservación Rural") {
-
+        var colum = search("preproduccion:NameColumns", 'proteccionconservacionrural');
+        var sel = search("preproduccion:selallproteccionconservacionrural");
     } else if (capas.target.innerHTML === "Red Acueducto Primaria") {
-
+        var colum = search("preproduccion:NameColumns", 'redacueductoprimaria');
+        var sel = search("preproduccion:selallredacueductoprimaria");
     } else if (capas.target.innerHTML === "Red Alcantarillado") {
-
+        var colum = search("preproduccion:NameColumns", 'redalcantarillado');
+        var sel = search("preproduccion:selallredalcantarillado");
     } else if (capas.target.innerHTML === "Red Energia Alta Tensión") {
-
+        var colum = search("preproduccion:NameColumns", 'redesenergiaaltatension');
+        var sel = search("preproduccion:selallredesenergiaaltatension");
     } else if (capas.target.innerHTML === "Ronda Arroyos 15m") {
-
+        var colum = search("preproduccion:NameColumns", 'rondasarroyossecundarios15m');
+        var sel = search("preproduccion:selallrondasarroyossecundarios15m");
     } else if (capas.target.innerHTML === "Ronda Arroyos 30m") {
-
+        var colum = search("preproduccion:NameColumns", 'rondaarroyosprincipales30m');
+        var sel = search("preproduccion:selallrondaarroyosprincipales30m");
     } else if (capas.target.innerHTML === "Ronda Río Magdalena") {
-
+        var colum = search("preproduccion:NameColumns", 'rondariomagdalena');
+        var sel = search("preproduccion:selallrondariomagdalena");
     } else if (capas.target.innerHTML === "Tratamientos Urbanisticos") {
-
+        var colum = search("preproduccion:NameColumns", 'tratamientosurbanisticos');
+        var sel = search("preproduccion:selalltratamientosurbanisticos");
     } else if (capas.target.innerHTML === "Vias Rural Propuesta") {
-
+        var colum = search("preproduccion:NameColumns", 'viasruralpropuesta');
+        var sel = search("preproduccion:selallviasruralpropuesta");
     } else if (capas.target.innerHTML === "Vias Transmasivo") {
-
+        var colum = search("preproduccion:NameColumns", 'vias_transmasivo');
+        var sel = search("preproduccion:selallvias_transmasivo");
     } else if (capas.target.innerHTML === "Zampa Ríos 50m") {
-
+        var colum = search("preproduccion:NameColumns", 'zampa_arroyos_principales_50m');
+        var sel = search("preproduccion:selallzampa_arroyos_principales_50m");
     } else if (capas.target.innerHTML === "Zampa Arroyos 30m") {
-
+        var colum = search("preproduccion:NameColumns", 'zampa_arroyos_secundarios_30m');
+        var sel = search("preproduccion:selallzampa_arroyos_secundarios_30m");
     } else if (capas.target.innerHTML === "Zampa Cienaga Mallorquin 50m") {
-
+        var colum = search("preproduccion:NameColumns", 'zampa_cienaga_mallorquin_50m');
+        var sel = search("preproduccion:selallzampa_cienaga_mallorquin_50m");
     } else if (capas.target.innerHTML === "Zampa Río Magdalena 50m") {
-
+        var colum = search("preproduccion:NameColumns", 'zampa_rio_magdalena_50m');
+        var sel = search("preproduccion:selallzampa_rio_magdalena_50m");
     } else if (capas.target.innerHTML === "Zona Declarada Prado") {
-
-    } else if (capas.target.innerHTML === "Zona Declarada Centro") {
-
+        var colum = search("preproduccion:NameColumns", 'zonadeclaradaprado');
+        var sel = search("preproduccion:selallzonadeclaradaprado");
+    } /*else if (capas.target.innerHTML === "Zona Declarada Centro") {}*/
+    var option = [];
+    for (i = 0; i < colum.length; i++) {
+        option[i] = document.createElement("option");
+        option[i].text = colum[i];
+        x.add(option[i], i);
+    }
+    for (j = 0; j < colum.length; j++) {
+        cell1 = row.insertCell(j);
+        cell1.innerHTML = colum[j];
+    }
+    for (j = 0; j < sel.length; j++) {
+        var row = table.insertRow(j + 1);
+        for (k = 0; k < sel[0].length; k++) {
+            cell1 = row.insertCell(k);
+            cell1.innerHTML = sel[j][k];
+        }
     }
 }
